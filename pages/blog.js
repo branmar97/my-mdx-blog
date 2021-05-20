5ae376d27ef147ce03b8eeb6bc3be5603bda3488
branmar97 <brandonmar97@gmail.com>
2021-05-20 12:42:15 -0400

    add posts to blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import { promises as fs } from 'fs'
 import path from 'path'
 import grayMatter from 'gray-matter'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
     return (
         <div>
             <Head>
@@ -17,16 +18,47 @@ const Blog = () => {
                     <small>Take a look at my posts</small>
                 </div>
                 <div>
-                    <div>
-                        Post 1
-                    </div>
-                    <div>
-                        Post 2
-                    </div>
+                    {posts.map(post => {
+                        return (
+                            <div>
+                                <Link key={post.path} href={post.path}><a><h2>{post.title}</h2></a></Link>
+                                <p>{post.summary}</p>
+                            </div>
+                        )
+                    })}
                 </div>
             </main>
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const postsDirectory = path.join(process.cwd(), 'pages/blog');
+    const filenames = await fs.readdir(postsDirectory);
+
+    const files = await Promise.all(filenames.map(async filename => {
+        const filePath = path.join(postsDirectory, filename)
+        const content = await fs.readFile(filePath, 'utf8')
+        const matter = grayMatter(content)
+        return {
+        filename, 
+        matter
+        }
+    }))
+
+    const posts = files.map(file => {
+        return {
+        path: `/blog/${file.filename.replace('.mdx', '')}`,
+        title: file.matter.data.title,
+        summary: file.matter.data.description
+        }
+    })
+
+    return {
+        props: {
+        posts
+        }
+    }
 }
 
 export default Blog;
