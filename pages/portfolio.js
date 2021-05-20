08e426f50d82e58f936ada3fbc6ecac6860e9aee
branmar97 <brandonmar97@gmail.com>
2021-05-20 12:22:13 -0400

    add portfolio boilerplate

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,7 +1,30 @@
+import Head from 'next/head'
+
 const Portfolio = () => {
     return (
         <div>
-            Portfolio
+            <Head>
+                <title>Brandon Marrero | Portfolio</title>
+                <meta name="description" content="Brandon Marrero Portfolio" />
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+            </Head>
+            <main>
+                <div>
+                    <h1>Portfolio</h1>
+                    <small>Take a look at my work</small>
+                </div>
+                <div>
+                    <div>
+                        Project 1
+                    </div>
+                    <div>
+                        Project 2
+                    </div>
+                    <div>
+                        Project 3
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
