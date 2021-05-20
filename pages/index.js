1351b75b79e5394c3517de2e08e4165f102418c3
branmar97 <brandonmar97@gmail.com>
2021-05-20 12:41:55 -0400

    remove posts from index page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import { promises as fs } from 'fs'
-import path from 'path'
-import grayMatter from 'gray-matter'
 import { SocialIcon } from 'react-social-icons'
 import styles from '../styles/Home.module.css'
-import { fileURLToPath } from 'url'
 
 export default function Home({ posts }) {
   return (
@@ -19,13 +15,19 @@ export default function Home({ posts }) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello, I'm <a href="#">Brandon!</a>
+          Hello, I'm <Link href="/about"><a>Brandon!</a></Link>
         </h1>
 
         <p className={styles.description}>
           <code className={styles.code}>Software Engineer</code>
           <code className={styles.code}>Content Creator</code>
         </p>
+
+        <div className={styles.nav}>
+            <div className={styles.navitem}><Link href="/about"><a>About</a></Link></div>
+            <div className={styles.navitem}><Link href="/portfolio"><a>Portfolio</a></Link></div>
+            <div className={styles.navitem}><Link href="/blog"><a>Blog</a></Link></div>
+        </div>
 
         <Image className={styles.portrait} src="/pic.jpeg" alt="Portrait Photo" width={250} height={250} />
 
@@ -35,54 +37,7 @@ export default function Home({ posts }) {
           <SocialIcon url="https://twitter.com/branmar97" bgColor="#0070f3" className={styles.icon} />
         </div>
 
-        <div className={styles.nav}>
-            <div className={styles.navitem}><Link href="/about"><a>About</a></Link></div>
-            <div className={styles.navitem}><Link href="/portfolio"><a>Portfolio</a></Link></div>
-            <div className={styles.navitem}><Link href="/blog"><a>Blog</a></Link></div>
-        </div>
-
-        <div className={styles.grid}>
-          {posts.map(post => {
-            return (
-              <Link key={post.path} href={post.path}>
-                <a className={styles.card}>
-                  <h2>{post.title}</h2>
-                  <p>{post.summary}</p>
-                </a>
-              </Link>
-            )
-          })}
-        </div>
       </main>
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const postsDirectory = path.join(process.cwd(), 'pages/blog');
-  const filenames = await fs.readdir(postsDirectory);
-
-  const files = await Promise.all(filenames.map(async filename => {
-    const filePath = path.join(postsDirectory, filename)
-    const content = await fs.readFile(filePath, 'utf8')
-    const matter = grayMatter(content)
-    return {
-      filename, 
-      matter
-    }
-  }))
-
-  const posts = files.map(file => {
-    return {
-      path: `/blog/${file.filename.replace('.mdx', '')}`,
-      title: file.matter.data.title,
-      summary: file.matter.data.description
-    }
-  })
-
-  return {
-    props: {
-      posts
-    }
-  }
 }
