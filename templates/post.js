7274ce3101528649e7b3add9c03f0ac22b000347
branmar97 <brandonmar97@gmail.com>
2021-05-21 10:42:45 -0400

    add styles to post page

--- templates/post.js
+++ templates/post.js
@@ -1,16 +1,18 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import styles from '../styles/Post.module.css'
 
 const Post = ({ children, frontMatter }) => {
     const { title, date } = frontMatter;
     
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <title>Brandon Marrero | {title}</title>
             </Head>
 
-            <main>
+            <main className={styles.main}>
+                <div className={styles.header}>
                 <small>
                     <Link href="/blog">
                         <a>
@@ -19,20 +21,24 @@ const Post = ({ children, frontMatter }) => {
                     </Link>
                 </small>
                 <h1>{title}</h1>
-                <div>
+                </div>
+                <div className={styles.postInfo}>
                     <hr />
                     <div>
-
-                    </div>
-                    <div>
-                        <h3>Brandon Marrero</h3>
-                        <small>Published on <strong>{date}</strong></small>
-
+                        <div className={styles.authorInfo}>
+                            <div>
+                                <img className={styles.authorImage} src="/pic.jpeg" alt="Portrait Photo" width="75" />
+                            </div>
+                            <div>
+                                <h3>Brandon Marrero</h3>
+                                <p>Published on <strong>{date}</strong></p>
+                            </div>
+                        </div>
                     </div>
                     <hr />
                 </div> 
                 
-                <div>
+                <div className={styles.postContent}>
                     {children}
                 </div>
             </main>
