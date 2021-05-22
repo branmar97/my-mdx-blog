de90439d5be6a03c003b149c252c5b6b819c8930
branmar97 <brandonmar97@gmail.com>
2021-05-22 10:12:44 -0400

    add basic styles to portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
+import styles from '../styles/Portfolio.module.css'
 
 const Portfolio = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <title>Brandon Marrero | Portfolio</title>
                 <meta name="description" content="Brandon Marrero Portfolio" />
                 {/* <link rel="icon" href="/favicon.ico" /> */}
             </Head>
-            <main>
+            <main className={styles.main}>
                 <div>
                     <h1>Portfolio</h1>
-                    <small>Take a look at my work</small>
+                    <small>I build projects to help strengthen my knowledge and skills in Ruby, JavaScript and other technologies.</small>
                 </div>
                 <div>
                     <div>
