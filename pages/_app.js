fe10fe70187eee794462a281ab307057e7545781
branmar97 <brandonmar97@gmail.com>
2021-05-24 16:01:57 -0400

    add nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container">
+        <div className="navbar">
+          <div className="logo">
+            <Link href="/"><a>BEM</a></Link>
+          </div>
+          <div className="nav">
+            <div className="nav-item"><Link href="/about"><a>About</a></Link></div>
+            <div className="nav-item"><Link href="/portfolio"><a>Portfolio</a></Link></div>
+            <div className="nav-item"><Link href="/blog"><a>Blog</a></Link></div>
+          </div>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
