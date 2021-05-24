49c78399998a1b9ce99550647e68bf61898bfcba
branmar97 <brandonmar97@gmail.com>
2021-05-24 19:43:30 -0400

    remove nav from app

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,7 @@ import '../styles/globals.css'
 import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="container">
-        <div className="navbar">
-          <div className="logo">
-            <Link href="/"><a>BEM</a></Link>
-          </div>
-          <div className="nav">
-            <div className="nav-item"><Link href="/about"><a>About</a></Link></div>
-            <div className="nav-item"><Link href="/portfolio"><a>Portfolio</a></Link></div>
-            <div className="nav-item"><Link href="/blog"><a>Blog</a></Link></div>
-          </div>
-      </div>
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
